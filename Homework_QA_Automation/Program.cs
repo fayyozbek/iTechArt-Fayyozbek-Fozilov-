a9d45ae40d5f1f66a581d96fcea0bc0f718ef4b6
fayyozbek <pazilovfa@gmail.com>
2022-12-25 21:58:57 +0500

    homework1

---
+++ Homework_QA_Automation/Program.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+class Program{
+
+    static void Main(string[] arg){
+       
+        
+
+
+    }
+}
