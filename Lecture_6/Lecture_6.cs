6f18fac708acfb4f405653d61207794cd62fd58c
fayyozbek <pazilovfa@gmail.com>
2022-12-28 21:24:50 +0500

    add space

--- Lecture_6/Lecture_6.cs
+++ Lecture_6/Lecture_6.cs
@@ -1,21 +1,26 @@
-﻿
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+
 class Program{
+    
     [Test]
     public void Test(){
+        
        //step 1
        IWebDriver driver= new ChromeDriver();//opening Chrome browser
        driver.Url="https://uzmarketing.com/";// move to website by link
+       
        //step 2
        driver.FindElement(By.XPath("//strong[text()='Services']")).Click();// looking for strong which text equal to "Services" and click it
        IWebElement element= driver.FindElement(By.XPath("//a[contains(@href, '/services/marketing-tools/')]"));// looking for hyperlink which href contains the link to marketing tools  
        Console.WriteLine(element.Text); // show expeted result
+       
        //step 3
        driver.FindElement(By.ClassName("fa-lightbulb-o")).Click();// find object by class name and click it
        element= driver.FindElement(By.XPath("//h4[text()='Graphic Design']"));// looking for h4 which text equal to "Graphic Design"
        Console.WriteLine(element.Text); // show expeted result
+       
        // step 4
        driver.FindElement(By.XPath("//strong[text()='Contact']")).Click();// looking for strong which text equal to "Contact" and click it
        element= driver.FindElement(By.XPath("//span[contains(text(), 'Fax')]"));// looking for span which text contains 'fax'
