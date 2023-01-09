b815e6143b3e416fa809d95f9caad1bcebf57066
fayyozbek <pazilovfa@gmail.com>
2023-01-09 17:31:05 +0500

    Add bdd selenium test

--- StudyingApproachesToBuildingTests/BDDTests/SwagLabsFunctions.feature
+++ StudyingApproachesToBuildingTests/BDDTests/SwagLabsFunctions.feature
@@ -3,4 +3,39 @@ Feature: Login
 	As user
 	I want to login to website 
 	
-Scenario: Login
+Scenario: Successful Login
+	Given I go to website https://www.saucedemo.com
+	And I fill in form username as standard_user
+	And I fill in form password as secret_sauce
+	And I click on button login
+	When I login to website
+	Then I see label products
+	
+Feature: Add item
+In oder to use add item to cart function
+As user
+I want to add item to cart 
+	
+	Scenario: Successful Add Item To Cart
+		Given I go to website https://www.saucedemo.com
+		And I fill in form username as standard_user
+		And I fill in form password as secret_sauce
+		And I click on button login
+		And I click on button add to cart
+		When I go to cart
+		Then I see one item added to cart
+		
+Feature: Remove item
+In oder to use remove item to cart function
+As user
+I want to remove item from cart 
+	
+	Scenario: Successful Add Item To Cart
+		Given I go to website https://www.saucedemo.com
+		And I fill in form username as standard_user
+		And I fill in form password as secret_sauce
+		And I click on button login
+		And I click on button add to cart
+		When I go to cart
+		And I click on button remove
+		Then I see empty cart
---
+++ StudyingApproachesToBuildingTests/SwagLabsFunctionsSteps.cs
@@ -0,0 +1,73 @@
+using TechTalk.SpecFlow;
+
+namespace StudyingApproachesToBuildingTests;
+
+[Binding]
+public class SwagLabsFunctionsSteps
+{
+    [Given(@"I go to website https://www\.saucedemo\.com")]
+    public void GivenIGoToWebsiteHttpsWwwSaucedemoCom()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"I fill in form username as standard_user")]
+    public void GivenIFillInFormUsernameAsStandardUser()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"I fill in form password as secret_sauce")]
+    public void GivenIFillInFormPasswordAsSecretSauce()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"I click on button login")]
+    public void GivenIClickOnButtonLogin()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"I login to website")]
+    public void WhenILoginToWebsite()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"I see label products")]
+    public void ThenISeeLabelProducts()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"I click on button add to cart")]
+    public void GivenIClickOnButtonAddToCart()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"I go to cart")]
+    public void WhenIGoToCart()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"I see one item added to cart")]
+    public void ThenISeeOneItemAddedToCart()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [When(@"I click on button remove")]
+    public void WhenIClickOnButtonRemove()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"I see empty cart")]
+    public void ThenISeeEmptyCart()
+    {
+        ScenarioContext.StepIsPending();
+    }
+}
