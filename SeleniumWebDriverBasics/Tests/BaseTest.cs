1dba3d35888be9b68ad23b0b1ce92a7d9ba3c9b6
fayyozbek <pazilovfa@gmail.com>
2023-02-02 14:37:59 +0500

    readd webdriver.quit

--- SeleniumWebDriverBasics/Tests/BaseTest.cs
+++ SeleniumWebDriverBasics/Tests/BaseTest.cs
@@ -29,9 +29,9 @@ public class BaseTest
         AuthenticationPage = new AuthenticationPage(WebDriver);
     }
 
-    // [TearDown]
-    // public void TearDown()
-    // {
-    //     WebDriver.Quit();
-    // }
+    [TearDown]
+    public void TearDown()
+    {
+        WebDriver.Quit();
+    }
 }
