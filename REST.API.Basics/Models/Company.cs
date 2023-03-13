cee663e567bcf61730b827ed98ab2db7a2c8e8ea
fayyozbek <pazilovfa@gmail.com>
2023-03-13 23:37:21 +0500

    Rest API Basics done

---
+++ REST.API.Basics/Models/Company.cs
@@ -0,0 +1,11 @@
+namespace REST.API.Basics.Models;
+
+public class Company
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    [JsonProperty("catchPhrase")]
+    public string CatchPhrase { get; set; }
+    [JsonProperty("bs")]
+    public string Bs { get; set; }
+}
